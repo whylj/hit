72abc1b80e33414af79555b4a1658e6b596c85f7
whylj <595272544@qq.com>
2018-01-16 23:27:49 +0800

    还原

--- package.js
+++ package.js
@@ -35,9 +35,9 @@ var enableDebugMode = function(game, enable) {
 
 var __main = function() {
     var images = {
-        ball: 'blob/master/img/ball.png',
-        block: 'blob/master/img/block.png',
-        paddle: 'blob/master/img/paddle.png',
+        ball: 'img/ball.png',
+        block: 'img/block.png',
+        paddle: 'img/paddle.png',
     }
     var game = GuaGame.instance(30, images, function(g){
         var s = SceneTitle.new(g)
